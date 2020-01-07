5b482c5650ca903b9044e4b4303983e818ef2fc3
Robert Laws <robertlaws@Roberts-MacBook-Pro.local>
2020-01-07 22:02:39 +0300

    basic file setup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
+
+import Hello from './Hello';
+
 import './App.scss';
 
 function App() {
-  const name = 'Hal';
-
-  function sayHello(name) {
-    return `Hello ${name}`;
-  }
-
-  function getNumber() {
-    return Math.floor(Math.random() * 10) + 1;
-  }
-
-  const getNum = getNumber();
-
   return (
-    <div className="App">
-      <p>Learn React, {sayHello(name)}</p>
-      <h2>Your number is... {getNum}</h2>
-      <h3>
-        {getNum === 7 ? 'Lucky' : 'Unlucky...'}
-      </h3>
-    </div>
+    <Hello />
   );
 }
 
