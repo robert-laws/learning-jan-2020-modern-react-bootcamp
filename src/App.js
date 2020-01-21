96f4b22eaac32fc901aeb93331901309dee44754
Robert Laws <roblaws@me.com>
2020-01-21 09:59:48 +0300

    clean up for next project - react hooks project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 
-// import Counter from './Counter';
-// import Toggler from './Toggler';
-// import SimpleForm from './SimpleForm';
-// import SimpleFormInputHook from './SimpleFormInputHook';
-// import Clicker from './Clicker';
-import StarWarsMovies from './StarWarsMovies';
+
 
 // styles
 import './sass/App.scss';
@@ -13,7 +8,7 @@ import './sass/App.scss';
 function App() {
   return (
     <div className="App">
-      <StarWarsMovies />
+      
     </div>
   );
 }
